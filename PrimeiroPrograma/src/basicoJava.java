d572b5b943ab45f932730fefad4e46308fdf284f
Alexy <alexygimenez@outlook.com>
2023-02-14 15:29:22 -0300

    atualizando exercicios

---
+++ PrimeiroPrograma/src/basicoJava.java
@@ -0,0 +1,15 @@
+public class basicoJava {
+    public static void main(String[] args){
+        byte idade = 30;
+        short ano = 2021;
+        int cep = 21515488;
+        long cpf = 15887999886L;
+        final float PI = 3.14F;
+        double salario = 2589.45;
+        String nomeCompleto = "Alexy Gimenez";
+        boolean sexoMasculino = true;
+        
+
+        System.out.println("hello world! ");
+    }
+}
